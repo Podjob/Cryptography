e32f7c7ee1c51daed7e24ae6b6943e40104b95b2
Podjob <71133117+Podjob@users.noreply.github.com>
2023-10-14 17:35:43 +0400

    Update VigenereCipher.java
    supro dupro bibidi babidi booooooom 💥💥💥💥💥

--- VigenereCipher/src/VigenereCipher.java
+++ VigenereCipher/src/VigenereCipher.java
@@ -11,17 +11,22 @@ public class VigenereCipher {
         String keyScan = scanner.nextLine();
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-
-        String encrypt = encrypt(cipherTextScan, keyScan, alphabet).toString();
+        String cipherTextLowerCase = textLowerCase(cipherTextScan).toString();
+        String keySequence = keySequenceString(keyScan, alphabet, cipherTextLowerCase).toString();
+        String encrypt = encrypt(alphabet, keySequence, cipherTextLowerCase).toString();
         System.out.println(encrypt);
-        System.out.println(decrypt(encrypt, keyScan, alphabet));
+        System.out.println(decrypt(alphabet, keySequence, encrypt));
     }
 
-    public static StringBuilder encrypt(String cipherText, String key, String alphabet){
-        StringBuilder keySequence = new StringBuilder();
-
+    public static StringBuilder textLowerCase(String text){
         StringBuilder cipherTextLowerCase = new StringBuilder();
-        cipherTextLowerCase.append(cipherText.toLowerCase());
+        cipherTextLowerCase.append(text.toLowerCase());
+        return cipherTextLowerCase;
+    }
+
+    public static StringBuilder keySequenceString(String key, String alphabet, String cipherTextLowerCase){
+
+        StringBuilder keySequence = new StringBuilder();
 
         for (int i = 0, numSymbolKey = 0; i < cipherTextLowerCase.length(); i++) {
             if(numSymbolKey == key.length()){
@@ -36,6 +41,11 @@ public class VigenereCipher {
                 keySequence.append('0');
             }
         }
+
+        return keySequence;
+    }
+
+    public static StringBuilder encrypt(String alphabet, String keySequence, String cipherTextLowerCase){
 
         StringBuilder encryptText = new StringBuilder();
 
@@ -57,32 +67,14 @@ public class VigenereCipher {
         return encryptText;
     }
 
-    public static StringBuilder decrypt(String cipherText, String key, String alphabet){
-        StringBuilder keySequence = new StringBuilder();
-
-        StringBuilder cipherTextLowerCase = new StringBuilder();
-        cipherTextLowerCase.append(cipherText.toLowerCase());
-
-        for (int i = 0, numSymbolKey = 0; i < cipherTextLowerCase.length(); i++) {
-            if(numSymbolKey == key.length()){
-                numSymbolKey = 0;
-            }
-            if(alphabet.indexOf(cipherTextLowerCase.charAt(i)) > -1){
-                keySequence.append(key.charAt(numSymbolKey));
-                numSymbolKey++;
-            }else if(cipherTextLowerCase.charAt(i) == ' '){
-                keySequence.append(' ');
-            }else{
-                keySequence.append('0');
-            }
-        }
+    public static StringBuilder decrypt(String alphabet, String keySequence, String encrypt){
 
         StringBuilder decryptText = new StringBuilder();
 
         for (int i = 0; i < keySequence.length(); i++) {
             if(alphabet.indexOf(keySequence.charAt(i)) > -1){
                 int numSymbolAlphabet = alphabet.indexOf(keySequence.charAt(i));
-                int numSymbolCipherText = alphabet.indexOf(cipherTextLowerCase.charAt(i));
+                int numSymbolCipherText = alphabet.indexOf(encrypt.charAt(i));
                 int shift = numSymbolCipherText - numSymbolAlphabet;
                 if(shift < 0){
                     shift += 26;
@@ -91,12 +83,14 @@ public class VigenereCipher {
             }else if(keySequence.charAt(i) == ' '){
                 decryptText.append(' ');
             }else if(keySequence.charAt(i) == '0'){
-                decryptText.append(cipherTextLowerCase.charAt(i));
+                decryptText.append(encrypt.charAt(i));
             }
         }
         return decryptText;
     }
 
-//    Hey! It's me! Ehehe~ I know you spent a long time trying yo find this huh? Or maybe not!
-//    mpkkirby
+//   текст                 : Hey! It's me! Ehehe~ I know you spent a long time trying yo find this huh? Or maybe not!
+//   ключ                  : mpkkirby
+//   преобразованный текст : tti! sb'j nc! qworm~ z llal iyc jqczi k vweh rubo dzpjls ny pqee rtxc rcy? pp ypilm epr!
+    
 }
